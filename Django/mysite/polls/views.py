ee6be72818aac989528abf76fe4da634cc2c6a76
pawanprjl <pawanparajuli8@gmail.com>
2019-06-25 09:17:35 +0545

    models for polls app added

--- Django/mysite/polls/views.py
+++ Django/mysite/polls/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Question
+
 
 # Create your views here.
 def index(request):
-    return HttpResponse("This is from polls app")
+    question = Question.objects.get(pk=1)
+    context = {
+        'choice_list': question.choice_set.all(),
+    }
+    return render(request, 'polls/index.html', context)
